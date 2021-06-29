e73fe8162415efe8f7de1082ad309233a06488c2
souma-web <80494095+souma-web@users.noreply.github.com>
2021-06-29 16:27:36 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'gmao',    
-        'USER': 'postgres', 
-        'PASSWORD': 'sogre1234',
+        'USER': '', 
+        'PASSWORD': '',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
